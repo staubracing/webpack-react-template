7f7294c2cbed5aa2aaf8254baabd85bff59457f5
StaubRacing <staubracing@gmail.com>
2023-12-01 13:22:34 -0600

    converted to react createroot

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react"; // import React from "react/react.js";
-import ReactDOM from "react-dom/client"; // import ReactDOM from "react-dom";
-import App from "./App"; 
+import { createRoot } from 'react-dom/client'; // import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css"; // import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
     <React.StrictMode>
         <App />
